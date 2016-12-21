a6c6e2b4695d7ce5ad9d4c028962d85a00c9bbb0
David M <davidmh.mail@gmail.com>
2016-12-20 21:06:17 -0700

    Assignment 8 - DOM Scripting: Animation - completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -2,6 +2,27 @@ var animatePoints = function() {
  
     var points = document.getElementsByClassName('point');
 
+    var revealPoint = function(ind) {
+        points[ind].style.opacity = 1;
+        points[ind].style.transform = "scaleX(1) translateY(0)";
+        points[ind].style.msTransform = "scaleX(1) translateY(0)";
+        points[ind].style.WebkitTransform = "scaleX(1) translateY(0)";
+    };
+    
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
+    
+};
+
+
+//Before refactoring
+
+/*
+var animatePoints = function() {
+ 
+    var points = document.getElementsByClassName('point');
+
     var revealFirstPoint = function() {
         points[0].style.opacity = 1;
         points[0].style.transform = "scaleX(1) translateY(0)";
@@ -28,3 +49,4 @@ var animatePoints = function() {
     revealThirdPoint();
 
 };
+*/
